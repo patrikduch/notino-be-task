a0b745672e05a164e89cbf4a7b47cd929b7b869a
patrikduch <duchpatrik@icloud.com>
2022-09-24 20:49:13 +0200

    Comment fix

--- NotinoBackendTask.Application/Models/EmailSettings.cs
+++ NotinoBackendTask.Application/Models/EmailSettings.cs
@@ -1,5 +1,15 @@
-﻿namespace NotinoBackendTask.Application.Models;
+﻿//---------------------------------------------------------------------------------
+// <copyright file="EmailSettings.cs" website="Patrikduch.com">
+//     Copyright (c) Patrik Duch, IČ: 09225471
+// </copyright>
+// <author>Patrik Duch</author>
+//---------------------------------------------------------------------------------
 
+namespace NotinoBackendTask.Application.Models;
+
+/// <summary>
+/// Email settings model for sending single e-mail.
+/// </summary>
 public class EmailSettings
 {
     public string ApiKey { get; set; } = string.Empty;
